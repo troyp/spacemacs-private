e559550e24b5c73cad0c668445f2e4f497b8e60c
Troy Pracy <troyp7@gmail.com>
2016-10-08 06:39:27 +1100

    add bb-spacemacs-layers to dotspacemacs-configuration-layer-path; add no-dots to dotspacemacs-configuration-layers

--- init.el
+++ init.el
@@ -13,7 +13,13 @@ values."
    dotspacemacs-distribution 'spacemacs
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '()
+
+   ;; dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layer-path
+   (list
+    (expand-file-name (file-name-as-directory "private/layer-groups/bb-spacemacs-layers")
+                      user-emacs-directory)
+    )
    ;; List of configuration layers to load. If it is the symbol `all' instead
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
@@ -75,6 +81,8 @@ values."
      (vinegar :variables
               vinegar-reuse-dired-buffer t)
      vimscript
+     ;; LOCAL LAYERS
+     no-dots
      troyp
      )
    ;; List of additional packages that will be installed without being
