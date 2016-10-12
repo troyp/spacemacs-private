afc74ed2ee1aefd602b6deda06ce35ff6d4f1012
Troy Pracy <troyp7@gmail.com>
2016-10-12 12:29:43 +1100

    update autogenerated code

--- init.el
+++ init.el
@@ -3060,6 +3060,7 @@ The window scope is determined by `avy-all-windows' (ARG negates it)."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-saved-nodes (quote ("(elisp)Top" "(eintr)Top")))
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.cache/bookmarks")
  '(exec-path-from-shell-arguments (quote ("-l")))
  '(paradox-automatically-star t))
