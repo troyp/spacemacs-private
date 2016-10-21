adce4ab176fd991da4c5634fc164b6a2dc8ec97f
Troy Pracy <troyp7@gmail.com>
2016-10-22 07:25:43 +1100

    remove customize autogen code from init.el

---
+++ init-customize.el
@@ -0,0 +1,23 @@
+
+
+;; Do not write anything past this comment. This is where Emacs will
+;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(Info-saved-nodes (quote ("(elisp)Top" "(eintr)Top")))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.cache/bookmarks")
+ '(exec-path-from-shell-arguments (quote ("-l")))
+ '(package-selected-packages
+   (quote
+    (undo-tree minitest insert-shebang hide-comnt ivy general evil-adjust xterm-color ws-butler wolfram-mode window-numbering wid-edit+ which-key web-mode web-beautify volatile-highlights vimrc-mode vi-tilde-fringe utop use-package ucs-cmds tuareg top-mode toc-org tiny thumb-frm thingatpt+ tagedit strings stan-mode spacemacs-theme spaceline smooth-scrolling smex smeargle slim-mode shm shell-pop scss-mode scad-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe restart-emacs replace+ rbenv ranger rainbow-delimiters racket-mode quelpa qml-mode pyvenv pytest pyenv-mode py-yapf pip-requirements persp-mode paradox palette page-break-lines orgit org-repo-todo org-present org-pomodoro org-plus-contrib org-bullets open-junk-file ocp-indent neotree names naked multi-term mozc move-text move-dup mmm-mode merlin matlab-mode markdown-toc magit-gitflow macrostep lua-mode ls lorem-ipsum linum-relative leuven-theme less-css-mode lacarte julia-mode json-mode js2-refactor js-doc jade-mode isearch-prop isearch+ info+ indent-guide ido-vertical-mode hy-mode hungry-delete htmlize hl-todo hindent highlight-parentheses highlight-numbers highlight-indentation help-mode+ help-fns+ help+ helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-make helm-gitignore helm-flx helm-firefox helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag haskell-snippets google-translate google-this golden-ratio go-eldoc gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger gh-md geiser font-lock+ fn flycheck-pos-tip flycheck-package flycheck-haskell flx-ido fish-mode firefox-controller fill-column-indicator fancy-battery facemenu+ eyedropper expand-region exec-path-from-shell evil-visualstar evil-visual-replace evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-args evil-anzu eshell-prompt-extras esh-help ensime emmet-mode elisp-slime-nav elfeed-web elfeed-org elfeed-goodies eclim doremi-mac doremi-frm doremi-cmd disaster dirtree dired-sort-menu+ dired+ define-word dactyl-mode cython-mode company-web company-tern company-statistics company-quickhelp company-go company-ghc company-cabal company-c-headers company-anaconda column-enforce-mode coffee-mode cmm-mode cmake-mode clj-refactor clean-aindent-mode clang-format cl-lib-highlight cider-eval-sexp-fu chruby bundler buffer-move bracketed-paste autofit-frame auto-yasnippet auto-highlight-symbol auto-compile arduino-mode aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell)))
+ '(paradox-automatically-star t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
