9f45b6862b38c7bd96943be2a208178d51d93215
Troy Pracy <troyp7@gmail.com>
2017-08-13 10:50:58 +1000

    Add yaml layer

--- init.el
+++ init.el
@@ -31,6 +31,7 @@ values."
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
    '(
+     yaml
      php
      csv
      ;; ----------------------------------------------------------------
