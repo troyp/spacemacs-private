803df673f6578ca0cc3c0140a6dcfdbe32963cea
Troy Pracy <troyp7@gmail.com>
2017-09-01 13:34:42 +1000

    Change to default startup banner

--- init.el
+++ init.el
@@ -272,7 +272,8 @@ values."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner "~/.emacs.d/core/banners/oo1-banner.txt"
+   ;; dotspacemacs-startup-banner "~/.emacs.d/core/banners/oo1-banner.txt"
+   dotspacemacs-startup-banner 'official
    ;; List of items to show in the startup buffer. If nil it is disabled.
    ;; Possible values are: `recents' `bookmarks' `projects'.
    ;; (default '(recents projects))
