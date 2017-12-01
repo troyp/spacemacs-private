682acd5b21d57ec120bda4509934457fccd94d90
Ivan Andrus <>
2017-12-01 12:13:48 +1100

    Advise find-file to handle line/column numbers format file:line:col
    Often used in error messages.
    
    from SO answer <https://stackoverflow.com/a/18026067/1261964>
    based on answer <https://stackoverflow.com/a/3141456/1261964>
    
    Co-authored-by: ecmanaut

--- init.el
+++ init.el
@@ -548,6 +548,34 @@ you should place you code here."
   (setq fci-rule-column 80)
   (setq fill-column 79)
 
+  ;; ,-----------,
+  ;; | Find File |
+  ;; '-----------'
+  ;; https://stackoverflow.com/a/18026067/1261964
+
+  ;; Open files and go places like we see from error messages, i e: path:line:col
+  ;; (to-do "make `find-file-line-number' check if the file exists")
+  (defadvice find-file (around find-file-line-number
+                               (path &optional wildcards)
+                               activate)
+    "Turn files like file.js:14:10 into file.js and going to line 14, col 10."
+    (save-match-data
+      (let* ((match (string-match "^\\(.*?\\):\\([0-9]+\\):?\\([0-9]*\\)$" path))
+             (line-no (and match
+                           (match-string 2 path)
+                           (string-to-number (match-string 2 path))))
+             (col-no (and match
+                          (match-string 3 path)
+                          (string-to-number (match-string 3 path))))
+             (path (if match (match-string 1 path) path)))
+        ad-do-it
+        (when line-no
+          ;; goto-line is for interactive use
+          (goto-char (point-min))
+          (forward-line (1- line-no))
+          (when (> col-no 0)
+            (forward-char (1- col-no)))))))
+
   ;; ,------------------,
   ;; | Spacemacs Config |
   ;; '------------------'
